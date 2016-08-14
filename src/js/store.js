fb7974ac463bb5745dfefe209bee76b9251b3a6a
Dawid Studziński <dawid@compsolutions.pl>
2016-08-14 18:03:40 +0200

    middleware attached

--- src/js/store.js
+++ src/js/store.js
@@ -1,7 +1,12 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import appReducers from './reducers';
+import logger from './middlewares/logger';
 
-const store = createStore(appReducers, window.devToolsExtension && window.devToolsExtension());
+const middlewares = applyMiddleware(logger);
+const store = createStore(appReducers, compose(
+  middlewares,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 export default store;
